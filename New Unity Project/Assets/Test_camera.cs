336bc228f8f7a95ff1a5d56603397cf4d64fa6ea
平野 <atomic_rolo@i.softbank.jp>
2018-05-01 10:47:26 +0900

    ステージとか色々と

---
+++ New Unity Project/Assets/Test_camera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_camera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("up"))
+        {
+            transform.position += transform.forward * 0.2f;
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.position += transform.forward * -0.2f;
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(0, 5, 0);
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.Rotate(0, -5, 0);
+        }
+    }
+}
